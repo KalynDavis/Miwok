f6cf7dee3820b4ec8e36c0349e6f51ec9fe2e102
Kalyn Davis <kalynwdavis@gmail.com>
2018-05-31 16:27:10 -0500

    Added intents for TextViews

--- app/src/main/java/com/kalyndavis/miwok/FamilyActivity.java
+++ app/src/main/java/com/kalyndavis/miwok/FamilyActivity.java
@@ -3,7 +3,7 @@ package com.kalyndavis.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class FamilyMembers extends AppCompatActivity {
+public class FamilyActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/kalyndavis/miwok/NumbersActivity.java
+++ app/src/main/java/com/kalyndavis/miwok/NumbersActivity.java
@@ -3,7 +3,7 @@ package com.kalyndavis.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Numbers extends AppCompatActivity {
+public class NumbersActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
